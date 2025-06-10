917b245b535c4fcc8c96f9b21f365497a4c1debb
hhpsoftwaredev <faldemir589@gmail.com>
2025-06-10 19:02:33 +1200

    first commit

---
+++ keys.js
@@ -0,0 +1,6 @@
+// List of private keys to use for bot
+module.exports = {
+    keys: [
+        "31Xopy3QG2vuvR9o3xuoDmQ8rJGcXwLPtg2c43agE4SuTQkbTRvUrLonHYwtks9LARjBNcGdXv5SdHYxA5q8C7SM",
+    ]
+}
